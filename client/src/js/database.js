03cfb9bb1362f56f9948e5a6f9d01c33f875b145
byxz885@gmail.com <53200767+byzESC@users.noreply.github.com>
2023-03-01 22:54:32 -0800

    first test good

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('posting to jate database');
+
+  const jateDb = await openDB('jate', 1);
+
+  jateDb.transaction('jate', 'readwrite')
+    .objectStore('jate')
+    .add({ text: content })
+    .then((result) => console.log('data saved to the database', result))
+    .catch((err) => console.error('===Error===', err));
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('getting from jate database');
+
+  const jateDb = await openDB('jate', 1);
+
+  jateDb.transaction('jate', 'readonly')
+    .objectStore('jate')
+    .getAll()
+    .then((result) => console.log('saved text are', result))
+    .catch((err) => console.error('===Error===', err));
+}
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -10,20 +10,69 @@ module.exports = () => {
   return {
     mode: 'development',
     entry: {
+      // include database.js?
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
+      index: './src/js/index.js'
     },
+
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor',
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+  
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor',
+        description: 'Simple Text Editor',
+        background_color: '#473C33',
+        theme_color: '#473C33',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-proposal-object-rest-spread', 
+                '@babel/transform-runtime'
+              ],
+            }
+          }
+        }
       ],
     },
   };
